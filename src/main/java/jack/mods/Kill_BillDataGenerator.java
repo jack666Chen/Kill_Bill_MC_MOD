0ad855a2b03d5d0a5eeb38eba6605c0463f94de6
jackchen <156235038+jack666Chen@users.noreply.github.com>
2025-04-25 19:20:24 -0400

    Template file uploaded

---
+++ src/main/java/jack/mods/Kill_BillDataGenerator.java
@@ -0,0 +1,11 @@
+package jack.mods;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Kill_BillDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
