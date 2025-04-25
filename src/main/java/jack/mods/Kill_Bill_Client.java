2337d84cda2c6f494c779d05c3515bf6b6dc631e
Ziang Chen <jackchen050802@gmail.com>
2025-04-25 19:55:28 -0400

    set up

---
+++ src/main/java/jack/mods/Kill_Bill_Client.java
@@ -0,0 +1,10 @@
+package jack.mods;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Kill_Bill_Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
